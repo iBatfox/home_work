b9fd968fbd07f90ee9a4dea9f8e0f7e55359f2e5
iBatfox <bataev.co@gmail.com>
2021-02-27 14:26:08 +0200

    home_work

---
+++ arrays_loops_tasks/4.php
@@ -0,0 +1,16 @@
+<?php
+$arr = [
+    'green'=>'зеленый',
+    'red'=>'красный',
+    'blue'=>'голубой',
+];
+
+echo "Столбец ключуй". PHP_EOL;
+foreach ($arr as $key => $value) {
+    echo $key. PHP_EOL;
+}
+
+echo "Столбец элементов". PHP_EOL;
+foreach ($arr as $value) {
+    echo $value. PHP_EOL;
+}
